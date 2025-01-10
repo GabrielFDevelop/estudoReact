e686e3b82891f0100f6dfd3ac2d05db3025766ac
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-01-10 14:51:58 -0300

    Frontend inicializado.

--- estudoReact1/frontend/src/App.tsx
+++ estudoReact1/frontend/src/App.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import {FiTrash} from 'react-icons/fi';
+import { api } from './services/api';
+
+interface CustomerProps{
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+}
 
 export default function App(){
+
+  const[ customers, setCustomers] = useState<CustomerProps[]>([]);
+
+  useEffect(() => {
+    loadCustomers();
+  }, [])
+
+  async function loadCustomers(){
+    const response = await api.get("/customers");
+
+    console.log(response.data);
+  }
+
   return(
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
       <main className="my-10 w-full md:max-w-2xl ">
@@ -15,15 +38,18 @@ export default function App(){
           <input type="submit" value="Cadastrar" className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"/>
         </form>
 
-        <section className="flex-col">
-          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
-            <p><span className="font-medium">Nome:</span> Matheus</p>
-            <p><span className="font-medium">Email:</span> teste@teste.com</p>
-            <p><span className="font-medium">Status:</span> ATIVO</p>
-            <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
-              <FiTrash size={18} color= "#fff" />
-            </button>
-          </article>
+        <section className="flex flex-col gap-4">
+
+          {customers.map((customer) => (
+            <article key={customer.id} className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+              <p><span className="font-medium">Nome:</span> {customer.name}</p>
+              <p><span className="font-medium">Email:</span> {customer.email}</p>
+              <p><span className="font-medium">Status:</span> {customer.status ? "Ativo" : "Inativo"}</p>
+              <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+                <FiTrash size={18} color= "#fff" />
+              </button>
+            </article>
+          ))}
         </section>
       </main>
     </div>
