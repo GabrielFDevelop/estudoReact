ac95153ad0cd274a345a4a0a4a0916f1a12d1b94
Gabriel Freitas <gabriel.freitas@bit.com.br>
2025-01-28 14:04:06 -0300

    Estudo sobre React.

---
+++ estudoReact2/4_eventos/src/components/EventHandlingExamples.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const EventHandlingExamples = () => {
+  return (
+    <div>
+        <button onClick={() => alert("olá")}>Clique aqui</button>
+        {/* minuto 53:41 */}
+    </div>
+  )
+}
+
+export default EventHandlingExamples;
